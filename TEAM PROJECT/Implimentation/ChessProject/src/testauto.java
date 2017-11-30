05d0c2f87b56bffbe2adb9cf0456fb8af0a18cce
Jay Patel <pateljay2197@gmail.com>
2017-11-30 15:40:01 -0500

    testing

--- TEAM PROJECT/Implimentation/ChessProject/src/testauto.java
+++ TEAM PROJECT/Implimentation/ChessProject/src/testauto.java
@@ -8,10 +8,14 @@ public class testauto {
 		
 		while (file.hasnext()) {
 		
-			bool test = detectchk(val 1, val 2)
+			bool test = detectchk()
 			
 			if (test==true) {
+			
 				print updatedboard()
+				
+				executemove(val 1, val 2);
+			
 			} else {
 				print error;
 			}
