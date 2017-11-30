4eac097fa9efe71cdad63cfa76655134ecd880e5
Jay Patel <pateljay2197@gmail.com>
2017-11-30 15:11:47 -0500

    It 4 updt

---
+++ TEAM PROJECT/Implimentation/ChessProject/src/testauto.java
@@ -0,0 +1,22 @@
+
+public class testauto {
+	/*
+	board Gameborad
+	main(){
+		
+		casestxtfile.in -> Scanner
+		
+		while (file.hasnext()) {
+		
+			bool test = detectchk(val 1, val 2)
+			
+			if (test==true) {
+				print updatedboard()
+			} else {
+				print error;
+			}
+			
+		}
+	}*/
+	
+}
